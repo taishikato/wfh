e04f733dba81fcad10f166874cb830a4803c4f68
taishikato <taishi.k0903@gmail.com>
2020-03-29 14:30:10 -0700

    save data on db

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,16 +4,25 @@ import { Button, Col, Form, Input, Row, Typography, Comment, Tooltip, Avatar } f
 import moment from 'moment'
 import { DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled } from '@ant-design/icons'
 // import { Store } from 'antd/lib/form/Form'
-const { Title } = Typography
+import firebase from '../plugins/firebase'
+import 'firebase/firestore'
 
+const db = firebase.firestore()
+
+const { Title } = Typography
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
 }
 
 const Home = () => {
-  const onFinish = (values: any) => {
+  const [form] = Form.useForm()
+  const onFinish = async (values: any) => {
     console.log(values)
+    await db.collection('declarations').add({
+      text: values.declaration,
+    })
+    form.resetFields()
   }
   return (
     <div>
@@ -23,8 +32,8 @@ const Home = () => {
           <Typography>
             <Title level={3}>What are going to do today?</Title>
           </Typography>
-          <Form {...layout} name="nest-messages" onFinish={onFinish}>
-            <Form.Item name="task">
+          <Form {...layout} form={form} name="nest-messages" onFinish={onFinish}>
+            <Form.Item name="declaration">
               <Input.TextArea />
             </Form.Item>
             <Form.Item>
