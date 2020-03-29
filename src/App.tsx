1f958808cb5b54409b915f39fe03aad07fd55656
taishikato <taishi.k0903@gmail.com>
2020-03-28 17:33:35 -0700

    Add some ant

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import React from 'react';
+import React from 'react'
 import Home from './routes/Home'
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css'
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Home />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <nav>navbar</nav>
+      <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import wfhdog from '../assets/img/wfhdog.gif'
-import { Button, Col, Form, Input, Row, Typography } from 'antd'
+import { Button, Col, Form, Input, Row, Typography, Comment, Tooltip, Avatar } from 'antd'
+import moment from 'moment'
+import { DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled } from '@ant-design/icons'
 // import { Store } from 'antd/lib/form/Form'
 const { Title } = Typography
 
@@ -16,21 +18,37 @@ const Home = () => {
   return (
     <div>
       <Row>
-        <img src={wfhdog} alt="dog" />
         <Col span={6} offset={6}>
+          <img src={wfhdog} alt="dog" />
           <Typography>
             <Title level={3}>What are going to do today?</Title>
-            <Form {...layout} name="nest-messages" onFinish={onFinish}>
-              <Form.Item name="task">
-                <Input.TextArea />
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
           </Typography>
+          <Form {...layout} name="nest-messages" onFinish={onFinish}>
+            <Form.Item name="task">
+              <Input.TextArea />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+          <Comment
+            // actions={actions}
+            author={<a>Han Solo</a>}
+            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Han Solo" />}
+            content={
+              <p>
+                We supply a series of design principles, practical patterns and high quality design resources (Sketch
+                and Axure), to help people create their product prototypes beautifully and efficiently.
+              </p>
+            }
+            datetime={
+              <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+                <span>{moment().fromNow()}</span>
+              </Tooltip>
+            }
+          />
         </Col>
       </Row>
     </div>
